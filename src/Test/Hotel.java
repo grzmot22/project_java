a433315da58bdb95debeebeef02dd607a69499fc
Grzmot22 <grzmot22@gmail.com>
2017-04-24 14:25:01 +0100

    Hotel: Add new constructors, create: reservationExist, createBooking, createReservation,
    Signed-off-by: Grzmot22 <grzmot22@gmail.com>

--- src/Test/Hotel.java
+++ src/Test/Hotel.java
@@ -1,9 +1,8 @@
 package Test;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.sql.*;
-import java.util.Objects;
+import java.sql.Date;
+import java.util.*;
 
 /**
  * Created by Grzmot22 on 2017-04-23.
@@ -23,11 +22,44 @@ public class Hotel {
     private int roomNo;
     private int userId;
     private Statement statement;
+    private ResultSet resultSet;
+    private boolean reservationIdAlreadyExist;
+    private Date startDate;
+    private Date endDate;
+    private int paidStatus;
 
-    public Hotel(int userId,  int reservationId,int roomNo) {
+    public Hotel(int userId,  int reservationId, int roomNo) {
+
         this.userId = userId;
         this.reservationId = reservationId;
         this.roomNo = roomNo;
+
+    }
+
+    public Hotel(int userId, int roomNo) {
+        this.userId = userId;
+        this.roomNo = roomNo;
+    }
+
+    public Hotel (int userId,  int reservationId, int roomNo, Date startDate, Date endDate, int paidStatus) {
+
+        this.userId = userId;
+        this.reservationId = reservationId;
+        this.roomNo = roomNo;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.paidStatus = paidStatus;
+
+    }
+
+    public Hotel (int userId, int roomNo, Date startDate, Date endDate, int paidStatus) {
+
+        this.userId = userId;
+        this.roomNo = roomNo;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.paidStatus = paidStatus;
+
     }
 
 
@@ -38,7 +70,7 @@ public class Hotel {
         try {
 
             statement = con.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM `reservations` WHERE reservation_id = '" + reservationId + "'");
+            resultSet = statement.executeQuery("SELECT * FROM `reservations` WHERE reservation_id = '" + reservationId + "'");
                 while (resultSet.next()) {
 
                     ResultSetMetaData metaData = resultSet.getMetaData();
@@ -68,17 +100,89 @@ public class Hotel {
         return book.toString();
     }
 
-    private void createBooking() {
+    private boolean reservationExist(){
 
-        setConnection();
         try {
-            Statement st = con.createStatement();
+
+            setConnection();
+
+            statement = con.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM `reservations` ");
+            int checkReservationId = -1;
+            boolean status = checkReservationId == reservationId;
+
+            while (!(checkReservationId == reservationId) ){
+
+                resultSet.next();
+                checkReservationId = resultSet.getInt("reservation_id");
+
+            }
+            return status;
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
+        return false;
     }
+    private void createBooking(int userId, Date startDate, Date endDate, int paidStatus) {
+
+        this.userId = userId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.paidStatus = paidStatus;
+
+        setConnection();
+        createReservation(userId);
+    }
+    private void createBooking(Date startDate, Date endDate, int paidStatus) {
+
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.paidStatus = paidStatus;
+
+        setConnection();
+        createReservation(this.userId);
+    }
+    private void createReservation(int userId){
+
+        try {
+            reservationIdAlreadyExist = false;
+            reservationExist();
+            statement = con.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM `reservations`");
+            reservationId = 1;
+
+            while (resultSet.next()){
+
+                int currentReservationId = resultSet.getInt("reservation_id");
+
+                if (currentReservationId > 0){
+                    if (reservationId == currentReservationId){
+                        reservationId = currentReservationId + 1;
+                    }
+                }
+            }
+
+            if (reservationExist()){
+                reservationIdAlreadyExist = true;
+                System.out.println("Login already exist");
+
+            } else if (!reservationExist() && !reservationIdAlreadyExist){
+
+                statement.executeUpdate("INSERT INTO `reservations` (`reservation_id`, `room_no`, `user_id`, `start_date`,`end_date`,`paid_status`) " +
+                        "VALUES ('"+reservationId+"', '"+roomNo+"', '"+userId+"', '"+startDate+"', '"+endDate+"', '"+paidStatus+"')");
+                System.out.println("Account has been created \n Login: "+reservationId+"\n Room No: "+roomNo+"\n UserId: "+userId
+                        +"\n Start date : "+startDate+"\n End date: "+endDate +"\n Payment status: "+paidStatus);
+
+            }
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+        }
+    }
+
 
 
     private String getBookedRooms() {
@@ -109,7 +213,10 @@ public class Hotel {
     }
     public static void main(String[] args) {
 
-        Hotel test = new Hotel(100001,20,101);
-        test.checkBooking();
+        Hotel test = new Hotel(100001,204);
+//        test.checkBooking();)
+        test.createBooking(1, Date.valueOf("2017-04-30"),Date.valueOf("2017-05-19"),1);
+//        System.out.println(test.reservationExist());
+
     }
 }
